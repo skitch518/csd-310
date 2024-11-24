e8ccfdaacc514e91236d4ce2acaee0fd29859dc6
skitch518 <jasonrschriner@gmail.com>
2024-11-23 21:41:58 -0500

    Pushing 11/23/24

--- module-6/mysql_test.py
+++ module-6/mysql_test.py
@@ -1,17 +1,15 @@
 import mysql.connector
 from mysql.connector import errorcode
-
 import dotenv
 from dotenv import dotenv_values
 
-secrets = dotenv_values(".env")
-
+secrets = dotenv_values('.env')
 config = {
     'user': secrets['USER'],
-    'password': secrets['PASSWORD'],
+    'password': secrets["PASSWORD"],
     'host': secrets['HOST'],     
     'database': secrets['DATABASE'], 
-    'raise_on_warnings': secrets['RAISE_ON_WARNING']
+    'raise_on_warnings': True
 }
 
 try:
@@ -22,7 +20,7 @@ try:
     input("\n\n Press any key to continue...")
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print(" The supplied username or passwor are invalid")
+        print(" The supplied username or password are invalid")
 
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
         print(" The specified database does not exist")
