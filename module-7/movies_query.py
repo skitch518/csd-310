c91b289b8aa1325d1710c5d8a252194bf584c46f
skitch518 <jasonrschriner@gmail.com>
2024-12-01 01:42:27 -0500

    Pushing 12/1/24

--- module-7/movies_query.py
+++ module-7/movies_query.py
@@ -17,7 +17,53 @@ try:
 
     print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
 
-    input("\n\n Press any key to continue...")
+    cursor = db.cursor()
+
+    query_one = '''
+    SELECT studio_id, studio_name FROM studio;'''
+
+    cursor.execute(query_one)
+
+    results_one = cursor.fetchall()
+    for row in results_one:
+        print(f"studio_id: {row[0]}, studio_name: {row[1]}")
+    
+    query_two = '''
+    SELECT genre_id, genre_name FROM genre;'''
+
+    cursor.execute(query_two)
+
+    print(" ")
+
+    results_two = cursor.fetchall()
+    for row in results_two:
+        print(f"genre_id: {row[0]}, genre_name: {row[1]}")
+    
+    
+    print(" ")
+    
+    query_three = """SELECT film_name, film_runtime
+    FROM film WHERE film_runtime < 120;"""
+
+    cursor.execute(query_three)
+
+    results_three = cursor.fetchall()
+    for row in results_three:
+        print(f"Film: {row[0]}, Runtime: {row[1]} minutes")
+    
+    print(" ")
+    
+    query_four = '''
+    SELECT film_name, film_director FROM film ORDER BY film_director, film_name;'''
+
+    cursor.execute(query_four)
+
+    print(" ")
+
+    results_four = cursor.fetchall()
+    for row in results_four:
+        print(f"film_name: {row[0]}, film_director: {row[1]}")
+    
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print(" The supplied username or password are invalid")
